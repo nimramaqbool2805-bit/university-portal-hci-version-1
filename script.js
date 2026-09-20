fee945ce603c264bc82cedd0ff1ad83a281f7c4b
nimramaqbool2805-bit <nimramaqbool2805@gmail.com>
2026-09-20 18:12:38 +0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function login(){
- const u=document.getElementById('username').value.trim();
- const p=document.getElementById('password').value.trim();
- const m=document.getElementById('message');
- if(!u||!p){m.textContent='ERROR: Please fill all required fields!';m.style.color='red';return;}
- m.textContent='Login successful! Redirecting...';m.style.color='green';
- setTimeout(()=>location.href='dashboard.html',500);
-}
